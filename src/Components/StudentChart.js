e60094ff76e47856de6d82808dba533bd6a6fab0
Richard van Esveld <Rfve@me.com>
2022-02-27 11:29:59 +0100

    Chart + Averages working

--- src/Components/StudentChart.js
+++ src/Components/StudentChart.js
@@ -1,61 +1,125 @@
-import { VictoryChart, VictoryGroup, VictoryLegend, VictoryStack, VictoryAxis, VictoryBar } from 'victory';
+import { VictoryChart, VictoryGroup, VictoryLegend, VictoryLabel, VictoryAxis, VictoryBar } from 'victory';
 import { StudentData } from '../Students/StudentData';
+
+const sum = (a, b) => {
+    return {
+        projectname: a.projectname,
+        hardshiplevel: a.hardshiplevel + b.hardshiplevel,
+        funlevel: a.funlevel + b.funlevel,
+    };
+};
+
+const getAverage = (array) => {
+    const sums = array.reduce(sum);
+    return {
+        projectname: sums.projectname,
+        hardshiplevel: sums.hardshiplevel / array.length,
+        funlevel: sums.funlevel / array.length,
+    };
+};
+
+const projectList = StudentData.map((student) => {
+    return student.projectname;
+});
+
+const projectListNew = projectList.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+});
+
+
+const allData = [];
+projectListNew.forEach((projectname) => {
+    const values = StudentData.filter((value) => {
+        return value.projectname === projectname;
+    });
+
+    allData.push(getAverage(values));
+});
+
 
 const StudentChart = () => {
     return (
         <VictoryChart
-
-            height={250}
+            height={180}
+            padding={20}
+            width={400}
         >
-            <VictoryLegend
 
-                x={50}
-                y={0}
-                orientation="horizontal"
-                gutter={20}
-                style={{ labels: { fontSize: 12, fill: "black" } }}
-            />
+            <VictoryGroup
+                offset={160 / StudentData.length}>
+
+                <VictoryBar
+                    data={allData}
+                    x={"projectname"}
+                    y={"hardshiplevel"}
+                    style={{ data: { fill: "#f81607" } }}
+                    barWidth={2}
+                // alignment="end"
+                // key={0}
+                />
+
+                <VictoryBar
+                    data={allData}
+                    x={"projectname"}
+                    y={"funlevel"}
+                    style={{ data: { fill: "#0f79e6" } }}
+                    barWidth={2}
+                // alignment="end"
+                // key={1}
+                />
+
+            </VictoryGroup>
 
             <VictoryAxis
-
+                tickValues={[1, 2, 3, 4, 5]}
                 label="Assignments"
-                tickValues={[1, 2, 3, 4, 5]}
+                style={{
+                    ticks: {
+                        fill: "transparent",
+                        size: 2,
+                    },
+                }}
+                tickLabelComponent={
 
+                    <VictoryLabel
+                        dx={-8.5}
+                        dy={-8}
+                        style={{
+                            fontSize: 4,
+                            fill: "#223243",
+                        }}
+                        textAnchor={"start"}
+                    />
+                }
             />
             <VictoryAxis
-
                 dependentAxis
+                domain={[0, 5]}
                 tickValues={[1, 2, 3, 4, 5]}
+                style={{
+                    tickLabels: {
+                        fontSize: 6,
+                        fill: "#223243",
+                    },
+                }}
+
                 label="Rating"
             />
 
-            <VictoryGroup
-                offset={20}>
+            <VictoryLegend
 
-                <VictoryStack
-                    style={{ data: { fill: "#f81607" } }}>
-                    <VictoryBar
-                        StudentData={StudentData}
-                        alignment="end"
-                        key={0}
-                        x="projectname"
-                        y="hardshiplevel"
-                    />
-                </VictoryStack>
+                x={20}
+                y={0}
+                orientation="horizontal"
+                // gutter={20}
+                style={{
+                    labels: {
+                        fontSize: 4,
+                        fill: "#120faa"
+                    }
+                }}
+            />
 
-                <VictoryStack offset={11}
-                    style={{ data: { fill: "#0f79e6" } }}>
-
-                    <VictoryBar
-                        StudentData={StudentData}
-                        alignment="end"
-                        key={1}
-                        x="projectname"
-                        y="funlevel"
-                    />
-                </VictoryStack>
-
-            </VictoryGroup>
         </VictoryChart>
     )
 }
